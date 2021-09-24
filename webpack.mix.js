f67eb7516042a18875d4d9586f55b18112b29fd6
nbutluo <nbutluo@163.com>
2021-09-24 10:51:55 +0800

    前台商品列表支持按类目筛选

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
-mix.js('resources/js/app.js', 'public/js')
+mix
+  .js("resources/js/app.js", "public/js")
   .vue()
-  .sass('resources/sass/app.scss', 'public/css')
+  .sass("resources/sass/app.scss", "public/css")
   .version();
