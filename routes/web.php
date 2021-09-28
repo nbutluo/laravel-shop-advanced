52f8be3283fcacc4bc54377e7619fdd8962917fb
nbutluo <nbutluo@163.com>
2021-09-28 14:05:37 +0800

    前台分期付款详情页

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
     Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
     Route::get('installments', 'InstallmentsController@index')->name('installments.index');
+    Route::get('installments/{installment}', 'InstallmentsController@show')->name('installments.show');
 
     Route::post('orders/{order}/received', 'OrdersController@received')->name('orders.received');
 
