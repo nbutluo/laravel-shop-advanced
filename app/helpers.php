47e8bb00ec42688ca7b690673324657dcebb6fb9
nbutluo <nbutluo@163.com>
2021-09-28 10:54:53 +0800

    初始化分期付款模型

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,7 @@
 <?php
 
-function test_helper() {
+function test_helper()
+{
     return 'OK';
 }
 
@@ -12,10 +13,16 @@ function route_class()
 function ngrok_url($routeName, $parameters = [])
 {
     // 开发环境，并且配置了 NGROK_URL
-    if(app()->environment('local') && $url = config('app.ngrok_url')) {
+    if (app()->environment('local') && $url = config('app.ngrok_url')) {
         // route() 函数第三个参数代表是否绝对路径
-        return $url.route($routeName, $parameters, false);
+        return $url . route($routeName, $parameters, false);
     }
 
     return route($routeName, $parameters);
+}
+
+// 默认的精度为小数点后两位
+function big_number($number, $scale = 2)
+{
+    return new \Moontoast\Math\BigNumber($number, $scale);
 }
